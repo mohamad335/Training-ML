54c6d9a07beccc6eb2958eca280472dd1e646e0f
mohammad alameen <mhamdl2amen@gmail.com>
2025-08-10 23:06:13 +0300

    the final step of preprocessing

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 dataset =pd.read_csv("sample_ml_data.csv")
 #x is the independent variable and y is the dependent variable
@@ -20,4 +22,10 @@ x = np.array(country.fit_transform(x))
 #now we can encode the dependent variable
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
-print(y)
+#now we can split the dataset into training set and test set
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+#now we can scale the features
+#we will scale the features except the first three columns
+sc = StandardScaler()
+x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
+x_test[:, 3:] = sc.transform(x_test[:, 3:])
